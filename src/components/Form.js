6dd2b5f0d7cbec3934e7c30297fd5c159c8a245b
NamTruong <nam.truongg2109@gmail.com>
2022-08-01 00:33:07 +0700

    update

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,11 +13,11 @@ function Form() {
 
   const onSubmit = () => {
     if (typeof quantity == "undefined" || typeof min == "undefined" || typeof max == "undefined"|| typeof name == "undefined") {
-      alert("Please complete all information.");
+      alert("Vui lòng điền đầy đủ thông tin!");
       return;
     }
     if (parseInt(max) < parseInt(min)) {
-      alert("Please enter min less than or equal to max");
+      alert("Vui lòng nhập số nhỏ nhất nhỏ hơn hoặc bằng số lớn nhất!");
       return;
     }
     navigate('/home', {state: {quantity, min, max, name}});
@@ -29,29 +29,29 @@ function Form() {
   };
 
   return (
-    <div className="mt-5">
+    <>
       <div className="form-group row mb-2">
-        <label htmlFor="name" className="col-sm-2 col-form-label">Name:</label>
+        <label htmlFor="name" className="col-sm-2 col-form-label">Tên:</label>
         <div className="col-sm-10">
           <input type="text" className="form-control" name="name" id="name" value={name}
                  onChange={e => setName(e.target.value)}/>
         </div>
       </div><div className="form-group row mb-2">
-        <label htmlFor="quantity" className="col-sm-2 col-form-label">Quantity:</label>
+        <label htmlFor="quantity" className="col-sm-2 col-form-label">Số lượng:</label>
         <div className="col-sm-10">
           <input type="number" className="form-control" name="quantity" id="quantity" value={quantity}
                  onChange={e => setQuantity(e.target.value)}/>
         </div>
       </div>
       <div className="form-group row mb-2">
-        <label htmlFor="min" className="col-sm-2 col-form-label">Min:</label>
+        <label htmlFor="min" className="col-sm-2 col-form-label">Số nhỏ nhất:</label>
         <div className="col-sm-10">
           <input type="number" className="form-control" name="min" id="min" value={min}
                  onChange={e => setMin(e.target.value)}/>
         </div>
       </div>
       <div className="form-group row mb-2">
-        <label htmlFor="max" className="col-sm-2 col-form-label">Max:</label>
+        <label htmlFor="max" className="col-sm-2 col-form-label">Số lớn nhất:</label>
         <div className="col-sm-10">
           <input type="number" className="form-control" name="max" id="max" value={max}
                  onChange={e => setMax(e.target.value)}/>
@@ -59,10 +59,10 @@ function Form() {
       </div>
       <div className="form-group row d-flex justify-content-center">
         <button className="btn btn-primary col-2 offset-2 btn-lg" onClick={onSubmit}>Random</button>
-        <button className="col-3 col-lg-2 btn btn-danger mx-2 btn-lg" onClick={onClickClearHistory}>Clear history</button>
+        <button className="col-3 col-lg-2 btn btn-danger mx-2 btn-lg" onClick={onClickClearHistory}>Xoá lịch sử</button>
       </div>
       <History items={itemsLocalStorage}/>
-    </div>
+    </>
   )
 }
 
--- src/components/History.js
+++ src/components/History.js
@@ -3,10 +3,10 @@ import React from "react";
 function History(props) {
   const {items} = props;
 
-  return <div className="fw-bolder mt-5 d-flex justify-content-center">History: <ul>{items.map((item, index) => {
+  return items.length > 0 ? <div className="fw-bolder mt-5 d-flex justify-content-center">Lịch sử: <ul>{items.map((item, index) => {
     return <li key={index}>{item.name}: {item.result.join(", ")}</li>
   })}
   </ul>
-  </div>
+  </div> : ""
 }
 export default History;
